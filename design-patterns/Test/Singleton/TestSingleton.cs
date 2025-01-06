49a46155d1439d2fbfb1febb89673a1dd34bbcd7
Alan Menchaca <alanmenchaca22@outlook.com>
2025-01-05 23:41:46 -0600

    add test singleton design pattern

---
+++ design-patterns/Test/Singleton/TestSingleton.cs
@@ -0,0 +1,18 @@
+﻿namespace design_patterns.Test.Singleton;
+
+using design_patterns.Singleton;
+
+public class TestSingleton
+{
+    [Fact]
+    private void TestSingletonNotNull()
+    {
+        Assert.NotNull(Singleton.GetSingleton());
+    }
+
+    [Fact]
+    private void TestIsSingleton()
+    {
+        Assert.Same(Singleton.GetSingleton(), Singleton.GetSingleton());
+    }
+}

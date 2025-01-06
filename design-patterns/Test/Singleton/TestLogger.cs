3d64baf99ff93aed9ee3998c52b3e08179ca0af1
Alan Menchaca <alanmenchaca22@outlook.com>
2025-01-05 23:42:54 -0600

    add test logger

---
+++ design-patterns/Test/Singleton/TestLogger.cs
@@ -0,0 +1,18 @@
+﻿using design_patterns.Singleton.Logger.Solution;
+
+namespace design_patterns.Test.Singleton;
+
+public class TestLogger
+{
+    [Fact]
+    public void TestSingletonNotNull()
+    {
+        Assert.NotNull(Logger.Instance);
+    }
+    
+    [Fact]
+    public void TestIsSingleton()
+    {
+        Assert.Same(Logger.Instance, Logger.Instance);
+    }
+}

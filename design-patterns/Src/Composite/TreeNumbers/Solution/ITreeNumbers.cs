7b08017a6a2f4afea357cbd4befed5c4a6cb3bac
Alan Menchaca <alanmenchaca22@outlook.com>
2025-01-07 02:25:26 -0600

    add tree numbers as composite design pattern

---
+++ design-patterns/Src/Composite/TreeNumbers/Solution/ITreeNumbers.cs
@@ -0,0 +1,10 @@
+﻿namespace design_patterns.Composite.TreeNumbers.Solution;
+
+public interface ITreeNumbers
+{
+    public void Add(ITreeNumbers child);
+    public void Remove(ITreeNumbers child);
+    public int NumberOfNodes();
+    public int Sum();
+    public int Higher();
+}

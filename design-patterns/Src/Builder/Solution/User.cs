bbcb507b319a99593cfd0e9d3e545343dbed4d08
Alan Menchaca <alanmenchaca22@outlook.com>
2025-01-12 14:25:34 -0600

    add builder design pattern

---
+++ design-patterns/Src/Builder/Solution/User.cs
@@ -0,0 +1,36 @@
+﻿namespace design_patterns.Builder.Solution;
+
+public class User
+{
+    private string? Id { get; }
+    private string? Nick { get; set; }
+    private string? Name { get; set; }
+    private string? FamilyName { get; set; }
+    private int Age { get; set; }
+    private int Phone { get; set; }
+    private string? Profession { get; set; }
+    private List<string?> Tags { get; set; }
+
+    public User() // empty
+    {
+    }
+
+    /*public static Builder UserBuilder(string id, string nick, string name)
+    {
+        return new Builder();
+    }*/
+
+    public bool TagContains(string tag)
+    {
+        return Tags.Contains(tag);
+    }
+
+    public class Builder
+    {
+        private readonly User _user;
+
+        private Builder()
+        {
+        }
+    }
+}

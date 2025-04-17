535faac265ae451f349bf6b9f79db07c6e267dd8
Alan Menchaca <alanmenchaca22@outlook.com>
2025-04-17 00:28:55 -0600

    feat: add TryParse, TryCatch, and DataTypes classes for number parsing and data type demonstration in C#

---
+++ c-sharp-course/TryCatch.cs
@@ -0,0 +1,36 @@
+﻿namespace c_sharp_course;
+
+public class TryCatch
+{
+    public static void TestTryCatchClause()
+    {
+        bool looping = true;
+        while (looping)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                // select .ToInt32 to see the possible exceptions
+                int num = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine($"You entered: {num}");
+                looping = false;
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+                Console.WriteLine("Please enter only numbers!\n");
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine(oe.Message);
+                Console.WriteLine($"Please enter a number less than {int.MaxValue}\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                
+            }
+        }
+    }
+}
---
+++ c-sharp-course/TryParse.cs
@@ -0,0 +1,27 @@
+﻿namespace c_sharp_course;
+
+public class TryParse
+{
+    public static void ParseANumber()
+    {
+        bool success = false;
+        
+        do
+        {
+            Console.Write("Enter a number: ");
+            string numInput = Console.ReadLine();
+
+            bool isParsed = int.TryParse(numInput, out int num);
+
+            if (isParsed)
+            {
+                success = true;
+                Console.WriteLine("The number is: " + num);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert!\n");
+            }
+        } while (!success);
+    }
+}

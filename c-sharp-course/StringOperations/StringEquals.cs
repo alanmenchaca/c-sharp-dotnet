1c751a5455cbc7c232b0ca8fd3b7ea9dac8f48ae
Alan Menchaca <alanmenchaca22@outlook.com>
2025-04-17 00:25:29 -0600

    feat: add string manipulation classes demonstrating character handling, string comparison, and formatting in C#

---
+++ c-sharp-course/StringOperations/StringEquals.cs
@@ -0,0 +1,44 @@
+﻿namespace c_sharp_course.StringOperations;
+
+public class StringEquals
+{
+    public static void CompareStrings()
+    {
+        string message = "Hello";
+        string compare = "Hello";
+
+        if (message.Equals(compare))
+        {
+            Console.WriteLine("The strings are equal");
+        }
+        else
+        {
+            Console.WriteLine("The strings are not equal");
+        }
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        if (!name.Equals(""))
+        {
+            Console.WriteLine($"Your name is {name}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid name input");
+        }
+
+        char[] chars = new char[] { 'H', 'e', 'l', 'l', 'o' };
+        object newCompare = new string(chars);
+
+        // comparing memory references not the actual values
+        if (message == newCompare)
+        {
+            Console.WriteLine("Same");
+        }
+        else
+        {
+            Console.WriteLine("Different");
+        }
+    }
+}
